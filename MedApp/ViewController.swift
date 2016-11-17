72608d37167ce1401ce1b52e2c257a3aba65cc3f
shrestha saroj <Shrestha@sarojs-macbook-pro.local>
2016-11-17 15:50:25 +0900

    Initial Commit

---
+++ MedApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedApp
+//
+//  Created by Saroj Shrestha on 11/17/16.
+//  Copyright © 2016 Saroj Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
